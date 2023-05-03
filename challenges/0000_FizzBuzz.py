47b011af2472aed89523237341a05cb98fe693e4
iamserda <iamserda@gmail.com>
2023-05-03 01:19:02 -0400

    added test

--- challenges/0000_FizzBuzz.py
+++ challenges/0000_FizzBuzz.py
@@ -28,6 +28,44 @@ def to_fizz_buzz_form(fizz:str="Fizz", buzz:str="Buzz", rg:int=100)-> None:
         b = i % 5 == 0 # True or False
         print( fizz_buzz if a and b else fizz if a else buzz if b else i)
 
-to_fizz_buzz_form() # uses default values: "Fizz", "Buzz", 100
-to_fizz_buzz_form(list(), dict(), "20") # "[]", "{}", "16", <- Weird Edge Case
-to_fizz_buzz_form("super", "mario", 31) # Super, Mario, SuperMario, i <- Reg Test
+
+# to_fizz_buzz_form() # uses default values: "Fizz", "Buzz", 100
+# to_fizz_buzz_form(list(), dict(), "20") # "[]", "{}", "16", <- Weird Edge Case
+# to_fizz_buzz_form("super", "mario", 31) # Super, Mario, SuperMario, i <- Reg Test
+
+# Testing...
+def test_to_fizz_buzz_form():
+    # Test case 1: Default values - fizz="Fizz", buzz="Buzz", rg=100
+    print("Test case 1:")
+    to_fizz_buzz_form()
+
+    # Test case 2: Edge case - fizz=[], buzz={}, rg="20"
+    print("\nTest case 2:")
+    to_fizz_buzz_form(list(), dict(), "20")
+
+    # Test case 3: Regular test - fizz="super", buzz="mario", rg=31
+    print("\nTest case 3:")
+    to_fizz_buzz_form("super", "mario", 31)
+
+    # Test case 4: fizz="Foo", buzz="Bar", rg=15
+    print("\nTest case 4:")
+    to_fizz_buzz_form("Foo", "Bar", 15)
+
+    # Test case 5: fizz="Baz", buzz="Qux", rg=50
+    print("\nTest case 5:")
+    to_fizz_buzz_form("Baz", "Qux", 50)
+
+    # Test case 6: fizz="Fuzz", buzz="Bizz", rg=25
+    print("\nTest case 6:")
+    to_fizz_buzz_form("Fuzz", "Bizz", 25)
+
+    # Test case 7: fizz="Hello", buzz="World", rg=10
+    print("\nTest case 7:")
+    to_fizz_buzz_form("Hello", "World", 10)
+
+    # Test case 8: fizz=123, buzz=456, rg=30
+    print("\nTest case 8:")
+    to_fizz_buzz_form(123, 456, 30)
+
+# Run the test
+test_to_fizz_buzz_form()
--- challenges/0001_CanVoteUS.py
+++ challenges/0001_CanVoteUS.py
@@ -7,29 +7,75 @@ class Citizen():
     def set_first_name(self, first_name: str):
         if first_name:
             self.first_name = first_name
-            return True
-        return False
+        if self.first_name == first_name:
+            return True if self.first_name == first_name else False
 
     def set_last_name(self, last_name: str):
         if last_name:
-            self.first_name = last_name
-            return True
-        return False
+            self.last_name = last_name
+            return True if self.last_name == last_name else False
 
     def set_age(self, age: int):
-        self.age = age
-        return True
+        if age is not None:
+            self.age = age
+            return True if self.age == age else False
 
 
 class Voter(Citizen):
+    """ Voter inherits from Citizen, hence we don't explicitly declare f, l, a"""
     def __init__(self, f: str = "", l: str = "", a: int = 0) -> None:
-        super().__init__(f, l, a)
-        self.can_vote = True if self.age >= 18 else False
+        super().__init__(f, l, a) # here we basically fire up the super classes constructor to initialize these values.
+        self.can_vote = True if self.age >= 18 else False # this is the only state that is created by the Voter constructor.
 
 
-citizen_1 = Voter("George", "Le Mieux", 63)
-citizen_2 = Voter("Genny", "La Grande", 8)
-print("Citizen:name:", citizen_1.first_name, citizen_1.last_name,
-      "Can Vote:", "Yes" if citizen_1.can_vote else "No")
-print("Citizen:name:", citizen_2.first_name, citizen_2.last_name,
-      "Can Vote:" "Yes" if citizen_2.can_vote else "No")
+def test_citizen_and_voter2():
+    # Test case 1: Create a Citizen instance with default values
+    citizen = Citizen()
+    assert citizen.first_name == ""
+    assert citizen.last_name == ""
+    assert citizen.age == 0
+
+    # Test case 2: Create a Citizen instance with custom values
+    citizen = Citizen("John", "Doe", 25)
+    assert citizen.first_name == "John"
+    assert citizen.last_name == "Doe"
+    assert citizen.age == 25
+
+    # Test case 3: Set first name and last name of Citizen
+    citizen = Citizen()
+    assert citizen.set_first_name("Alice") == True
+    assert citizen.set_last_name("Smith") == True
+    assert citizen.first_name == "Alice"
+    assert citizen.last_name == "Smith"
+
+    # Test case 4: Set age of Citizen
+    citizen = Citizen()
+    assert citizen.set_age(30) == True
+    assert citizen.age == 30
+
+    # Test case 5: Create a Voter instance with default values
+    voter = Voter()
+    assert voter.first_name == ""
+    assert voter.last_name == ""
+    assert voter.age == 0
+    assert voter.can_vote == False
+
+    # Test case 6: Create a Voter instance with custom values
+    voter = Voter("Jane", "Smith", 16)
+    assert voter.first_name == "Jane"
+    assert voter.last_name == "Smith"
+    assert voter.age == 16
+    assert voter.can_vote == False
+
+    # Test case 7: Create a Voter instance with age exactly 18
+    voter = Voter("Bob", "Doe", 18)
+    assert voter.first_name == "Bob"
+    assert voter.last_name == "Doe"
+    assert voter.age == 18
+    assert voter.can_vote == True
+
+    print("All test cases pass.")
+
+# Run the test
+test_citizen_and_voter2()
+
